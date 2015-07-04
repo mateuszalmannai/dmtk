209aa7ae43ea2432f71e6a70ac6e1d4c29a778ab
mateusz <mateuszalmannai@googlemail.com>
2015-07-04 17:13:54 +0200

    add specs and corresponding functionality

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,4 +1,5 @@
 class ToolsController < ApplicationController
   def index
+    @tools = ["Tool 1", "Tool 2", "Tool 3"]
   end
 end
--- spec/features/list_tools_spec.rb
+++ spec/features/list_tools_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 describe "Viewing the list of tools" do
   it "shows the tools" do
-    visit 'http://example.com/tools'
+    visit tools_url
 
     expect(page).to have_text("3 Tools")
+    expect(page).to have_text("Tool 1")
+    expect(page).to have_text("Tool 2")
+    expect(page).to have_text("Tool 3")
   end
 end
